07dfe1b690394c6097e5b1ac318df9360d52403c
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-04-15 22:30:49 +0200

    up

--- charac.py
+++ charac.py
@@ -1,35 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('Test20261504002.dat')
+data1 = np.loadtxt("./Data_KP_IDS_VDS/20261504001.dat")
+data2 = np.loadtxt("./Data_KP_IDS_VDS/20261504002.dat")
+data3 = np.loadtxt("./Data_KP_IDS_VDS/20261504003.dat")
 
-# Remove trailing placeholder rows where all values are zero.
-valid_rows = np.any(data != 0, axis=1)
-data = data[valid_rows]
-
-V_GS = data[:, 0]
-I_GS = data[:, 1]
-V_DS = data[:, 2]
-I_DS = data[:, 3]
-conductivity_gs = data[:, 4]
-resistivity_gs = data[:, 5]
-
-fig, axes = plt.subplots(3, 2, figsize=(12, 10), constrained_layout=True)
-
-plots = [
-	(V_GS, 'V_GS'),
-	(I_GS, 'I_GS'),
-	(V_DS, 'V_DS'),
-	(I_DS, 'I_DS'),
-	(conductivity_gs, 'Conductivity GS'),
-	(resistivity_gs, 'Resistivity GS'),
+datasets = [
+    ("5 V", data1, 'red'),
+    ("2 V", data2, 'blue'),
+    ("8 V", data3, 'green'),
 ]
 
-for ax, (series, title) in zip(axes.flat, plots):
-	ax.plot(V_GS, series, linewidth=1.5)
-	ax.set_title(title)
-	ax.set_xlabel('V_GS')
-	ax.set_ylabel(title)
-	ax.grid(True, alpha=0.3)
+for label, data, color in datasets:
+    V_DS = data[:, 0]
+    I_DS = data[:, 1]
+    plt.plot(V_DS, I_DS, lw=2, label=label, color=color)
 
+plt.xlabel("V_DS (V)")
+plt.ylabel("I_DS (A)")
+plt.title("Output Characteristics of MOSFET")
+plt.legend()
 plt.show()
