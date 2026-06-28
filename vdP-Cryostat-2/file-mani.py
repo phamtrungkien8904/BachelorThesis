91fec77a0e197f45b2f1747b2f22bf62c6c0caa1
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-06-28 13:36:17 +0200

    up

--- vdP-Cryostat-2/file-mani.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-folder = Path("./Data-20261006")   # change this to your folder path
-
-for file in folder.glob("*.txt"):
-    file.unlink()
-    print(f"Deleted: {file}")
