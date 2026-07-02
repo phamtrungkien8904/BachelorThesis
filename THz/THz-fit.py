091a8da906338529a6a760704d7762ca6b825e7d
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-07-02 12:54:03 +0200

    upupdate

--- THz/THz-fit.py
+++ THz/THz-fit.py
@@ -2,38 +2,71 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-# Custom settings
+# Custom settings for python figures
 plt.style.use('classic')
+
+# plt.rcParams.update({
+#     "text.usetex": True,
+#     'text.latex.preamble': r'''
+#     \usepackage[T1]{fontenc}
+#     \usepackage{lmodern}
+#     \usepackage[utf8]{inputenc}
+#     \usepackage{amsmath}
+#     \usepackage{amssymb}
+#     \usepackage{siunitx}
+#     \usepackage{sfmath}
+#     '''
+# })
 plt.rcParams.update({
-    'figure.dpi': 100,
-    'figure.figsize': (8, 6),
+    # Figure settings
+    'figure.dpi': 300,
+    # 'figure.figsize': (10/2.54, 6/2.54),  # 10x6 cm in inches (1 figure per line)
+    'figure.figsize': (8/2.54, 6/2.54),  # 10x6 cm in inches (2 figures per line)
     'figure.facecolor': 'white',
     'axes.facecolor': 'white',
     'axes.edgecolor': 'black',
-    'axes.linewidth': 2,
-    'axes.labelsize': 15,
+    'axes.linewidth': 1,
+    'axes.labelsize': 8,
+    'axes.titlesize': 8,
     'axes.labelcolor': 'black',
     'savefig.facecolor': 'white',
     'font.family': 'sans-serif',
-    'font.sans-serif': ['Arial'],
-    'mathtext.fontset': 'cm',
+    'font.sans-serif': 'Arial',
+    # 'mathtext.fontset': 'cm',
+    'figure.constrained_layout.use': True,
 
-    'savefig.bbox': 'tight',
     # Ticks
     "xtick.direction": "in",
     "ytick.direction": "in",
     "xtick.top": True,
     "ytick.right": True,
-    "xtick.major.size": 8,
-    "ytick.major.size": 8,
-    "xtick.major.width": 2,
-    "ytick.major.width": 2,
+    "xtick.major.size": 4,
+    "ytick.major.size": 4,
+    "xtick.major.width": 1,
+    "ytick.major.width": 1,
     "xtick.minor.visible": True,
     "ytick.minor.visible": True,
-    "xtick.minor.size": 4,
-    "ytick.minor.size": 4,
-    "xtick.minor.width": 1.5,
-    "ytick.minor.width": 1.5,
+    "xtick.minor.size": 0,
+    "ytick.minor.size": 0,
+    "xtick.minor.width":0,
+    "ytick.minor.width": 0,
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8,  
+
+    # Legend
+    'legend.frameon': False,
+    'legend.title_fontsize': 8,
+    'legend.fontsize': 8,
+    'legend.handlelength': 2,
+    'legend.loc': 'best',
+    'legend.numpoints': 1,
+
+    # Line style
+    'lines.linestyle': '-',
+    'lines.linewidth': 1,
+    'lines.markersize': 4,
+    'lines.markeredgecolor': 'white',
+    'lines.markeredgewidth': 0.5,
 })
 
 
@@ -45,7 +78,7 @@ epsilon = 11.7
 Z0 = 376.73
 Ref = 2*np.sqrt(epsilon)/Z0
 
-data = np.loadtxt('20260526_FreqDomain.dat')
+data = np.loadtxt('20260526001.dat')
 f = data[:, 0]
 Re = data[:, 1]* Ref
 Im = data[:, 2]* Ref
@@ -68,8 +101,9 @@ a_err = float(perr_1[0])
 def func_2(x, n2D):
     return a**2 *m/(n2D* e**2) * 2*np.pi*x 
 
-popt_2, pcov_2 = curve_fit(func_2, f, Im)
-Im_fit = func_2(f, *popt_2)
+mask = (f > 0.5e12) & (f < 3e12)
+popt_2, pcov_2 = curve_fit(func_2, f[mask], Im[mask])
+Im_fit = func_2(f[mask], *popt_2)
 n2D = float(popt_2[0])
 
 
